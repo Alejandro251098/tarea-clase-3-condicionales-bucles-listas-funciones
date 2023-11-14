a6071f0ee1de9eeb7800b3758419352bdbe2c5c8
Alejandro251098 <alejoramirez19@gmail.com>
2023-11-14 01:16:45 -0500

    tarea clase 3

---
+++ tareafunciones.py
@@ -0,0 +1,75 @@
+# EJERCICIO 2 :verificar paridad
+
+# def verificar_pares(numero):
+#     if (numero % 2) == 0: 
+#         print('Es par')
+#     else:
+#         print('Es impar')
+
+# verificar_pares(3) 
+
+
+# EJERCICIO 3 : DICCIONARIO DE ESTUDIANTES 
+
+
+# estudiantes = {}
+
+# def agregar(estudiante,nombre,edad, calificacion):
+
+#     estudiante[nombre] = {
+#         "edad":edad , 
+#         "calificacion":calificacion
+#     }        
+
+
+#     print(estudiante)
+
+
+# agregar(estudiantes,"alejandro" , 25 , 4.5)
+# agregar(estudiantes,"juan" , 22 , 4)
+# agregar(estudiantes,"camila" , 21 , 5)      
+
+# def elimina(estudiante, nombre):
+#     if nombre in estudiante:
+#         del estudiante[nombre]
+#         print("estudiante esliminado")
+#     else:
+#         print("no se encontro el estudiante")     
+#     print(estudiante)        
+
+# elimina(estudiantes , "juan")       
+
+
+
+# EJERCICIO 4 : INVERTIR LISTA 
+
+
+
+# def invertir_lista (lista) :
+#     lista.reverse()
+#     return lista
+
+
+# lista = [ "alejandro" , 25 , "camila" , 22 , "manolo" , 7 ,"sussy" , 4]
+# print(invertir_lista(lista))
+  
+  
+#  EJERCICIO 5 : VALIDACION DE PARENTESIS 
+
+def validar_parentesis(parentesis) :
+    validar =[] 
+    caracteres = { "(" : ")", "[" :"]" , "{":"}"}   
+    
+    for c in parentesis :
+        if c in caracteres :
+            validar.append(c)
+      
+        elif  len(caracteres) == 0 or c != caracteres[validar.pop()] :
+              return False
+          
+          
+    return len(validar) == 0
+
+print(validar_parentesis("([{}])"))
+print(validar_parentesis("(((())))"))
+print(validar_parentesis("()[]{}("))        
